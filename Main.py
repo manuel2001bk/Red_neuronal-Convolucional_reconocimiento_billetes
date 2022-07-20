dd2ea0905463443ec15d980f6b287e478e722696
manuel2001bk <61120758+manuel2001bk@users.noreply.github.com>
2022-07-19 22:09:26 -0500

    modelo final

--- Main.py
+++ Main.py
@@ -8,57 +8,59 @@ model = []
 historial = []
 
 
-
 def metodo2():
     data_gen = ImageDataGenerator(
-        rescale=1./255,
-        rotation_range=30,
-        width_shift_range=0.25,
-        height_shift_range=0.25,
-        shear_range=15,
-        zoom_range=[0.5, 1.5],
-        horizontal_flip=True,
-        validation_split=0.2
+        rotation_range=15,
+        width_shift_range=0.10,
+        height_shift_range=0.10,
+        validation_split=0.15
     )
     data_gen_entrenamiento = data_gen.flow_from_directory(
         f'Dataset',
-        target_size=(400, 600),
-        batch_size=32,
+        target_size=(150, 200),
+        batch_size=16,
         shuffle=True,
         subset='training')
     data_gen_pruebas = data_gen.flow_from_directory(
         f'Dataset',
-        target_size=(400, 600),
-        batch_size=32,
+        target_size=(150, 200),
+        batch_size=16,
         shuffle=True,
         subset='validation')
+    plt.figure(figsize=(20, 20))
     for imagen, etiqueta in data_gen_entrenamiento:
         for i in range(10):
+            print(etiqueta[i])
             plt.subplot(2, 5, i+1)
-            plt.imshow(imagen[i])
+            plt.imshow((imagen[i]/255))
         break
     plt.show()
+    plt.figure(figsize=(20, 20))
+
     for imagen, etiqueta in data_gen_pruebas:
         for i in range(10):
+            print(etiqueta[i])
             plt.subplot(2, 5, i+1)
-            plt.imshow(imagen[i])
+            plt.imshow(imagen[i]/255)
         break
     plt.show()
 
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu',
-                               input_shape=(400, 600, 3)),
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu',
+                               input_shape=(150, 200, 3)),
         tf.keras.layers.MaxPooling2D(2, 2),
 
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.MaxPooling2D(2, 2),
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D(2, 2),
 
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
-
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(100, activation='relu'),
-        tf.keras.layers.Dense(8, activation='softmax')
+        tf.keras.layers.Dense(50, activation='relu'),
+        tf.keras.layers.Dense(5, activation='softmax')
     ])
 
     model.compile(optimizer='adam',
